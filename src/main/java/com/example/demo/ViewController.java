ce4310c730e32bfdd0c3c3b46cf8425cd568a323
Tino Roivanen <50320109+Tinosaurus@users.noreply.github.com>
2022-10-25 15:17:33 +0300

    Initial commit for the software
    course work thingy

---
+++ src/main/java/com/example/demo/ViewController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/students")
+    public String ListStudents(Model model){
+        return "students";
+    }
+
+    @RequestMapping("/courses")
+    public String ListCourses(Model model){
+        return "courses";
+    }
+
+}
